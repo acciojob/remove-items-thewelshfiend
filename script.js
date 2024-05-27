4b140a6c42e309061c3c2e1a58d1fb8202ff5259
Kiran TJ <166934975+thewelshfiend@users.noreply.github.com>
2024-05-27 15:57:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+let arr = [`Red`, `Green`, `White`, `Black`];
+const selection = document.getElementById("colorSelect");
+renderDropDown();
+const options = selection.getElementsByTagName("option");
+const button = document.querySelector("input");
+
+function renderDropDown()
+{
+    selection.innerHTML = ``;   // Resets the list before rendering it
+    for (let color of arr)
+    {
+        selection.innerHTML += `<option value=${color}>${color}</option>`;
+    }
+    return;
+}
+
+button.addEventListener("click", selectAndRemove);
+function selectAndRemove()
+{
+    const color = selection.value;
+    arr = arr.filter(item => item != color);
+    renderDropDown();
+    return;
+}
